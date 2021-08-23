7d291ba147d90ea3eaf696161a327e74d54047da
henriquebcustodio <henriquebcustodio@outlook.com>
2021-08-22 22:19:45 -0300

    Added loading overlay

--- src/js/todo.js
+++ src/js/todo.js
@@ -1,3 +1,4 @@
+const loadingOverlay = document.querySelector('#loading');
 async function loadSideBar() {
     const collectionsList = await firCollectionsSnapshot();
     updateSidebar(collectionsList);
