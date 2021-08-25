ff20b20911550cdb7ae7220066ecbcf6c737ddd0
henriquebcustodio <henriquebcustodio@outlook.com>
2021-08-24 22:20:21 -0300

    Changed default route

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'frontend')));
 
-app.get('/app', (req, res) => {
+app.get('/', (req, res) => {
     res.render('app');
 });
 
